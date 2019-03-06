cc21b806f16e94397c0e6534c5176d53906e766c
Alexey Bokhovkin <alexey_bokhovkin@ubuntu.local>
2019-03-06 17:23:35 +0300

    sampling abc

--- src/backend/sampler/sampler.py
+++ src/backend/sampler/sampler.py
@@ -7,13 +7,20 @@ import math
 
 
 class MeshSampler:
-    def __init__(self, mesh):
+    def __init__(self, mesh, check_faces=False):
         self.mesh = mesh
         self.sphere_points = trimesh.sample.sample_surface(mesh.bounding_sphere, count=1000)[0]*2
         self.correct_faces = {i:0 for i in range(len(mesh.faces))}
         self.ray_mesh = RayMeshIntersector(geometry=mesh)
         self.faces_centroids = self.mesh.triangles.mean(axis=1)
         self.correct_points = np.array([])
+        if check_faces:
+            self.compute_visible_faces()
+        else:
+            self.correct_faces = {i:1 for i in range(len(mesh.faces))}
+    
+    def visible_faces(self):
+        return self.correct_faces
         
     def compute_visible_faces(self):
         for i, face in enumerate(tqdm(self.mesh.triangles)):
@@ -27,10 +34,13 @@ class MeshSampler:
     def sample_points(self, n_points=10000):
         points = trimesh.sample.sample_surface(self.mesh, count=n_points)
         correct_points = []
+        normals_for_points = []
         for i, point in enumerate(tqdm(points[0])):
             if self.correct_faces[points[1][i]] == 1:
                 correct_points += [point]
+                normals_for_points += [self.mesh.face_normals[points[1][i]]]
         self.correct_points = np.array(correct_points)
+        self.normals_for_points = np.array(normals_for_points)
         return self.correct_points
     
     def compute_sdf(self, sigma=0.0025):
@@ -39,10 +49,12 @@ class MeshSampler:
         sdf = self.mesh.nearest.signed_distance(noisy_points)
         correct_mesh_points = []
         correct_sdf = []
+        correct_normals = []
         for i, distance in enumerate(sdf):
             if math.isnan(distance):
                 continue
             else:
                 correct_mesh_points += [noisy_points[i]]
                 correct_sdf += [sdf[i]]
-        return np.array(correct_mesh_points), np.array(correct_sdf)
+                correct_normals += [self.normals_for_points[i]]
+        return np.array(correct_mesh_points), np.array(correct_sdf), np.array(correct_normals)
